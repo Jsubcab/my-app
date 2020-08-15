b2ff6c953c7daae74604406f4703d6f7db06f07a
Jorge Cabrera <jsubcab@gmail.com>
2020-08-15 21:23:29 +0200

    Updates

---
+++ precache-manifest.b97ad63c223f4be9a538530ec366669f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11c72488b833ba06c6fa9ff8c83edda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f15f76e0fb24b8be51",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6abef6cda98f93fde716",
+    "url": "/static/css/main.e662f9c4.chunk.css"
+  },
+  {
+    "revision": "23f15f76e0fb24b8be51",
+    "url": "/static/js/2.622c5517.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/static/js/2.622c5517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6abef6cda98f93fde716",
+    "url": "/static/js/main.0c2b747c.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "2961b500c9dac35354f162270b95d99a",
+    "url": "/static/media/github.2961b500.png"
+  },
+  {
+    "revision": "5e9be06757143a32b124d2a3d1c02681",
+    "url": "/static/media/jamming.5e9be067.png"
+  },
+  {
+    "revision": "a078f5c5d1dbfb5e0d7b9bfcd1d89e8f",
+    "url": "/static/media/juego-3.a078f5c5.png"
+  },
+  {
+    "revision": "cfd71af037582475fa30982fe4612ada",
+    "url": "/static/media/matching game.cfd71af0.png"
+  },
+  {
+    "revision": "217fe74b1fef7ab00d331d438be0316a",
+    "url": "/static/media/profile.217fe74b.png"
+  },
+  {
+    "revision": "08a1258d0a1e78cf0ecce9653accfb8b",
+    "url": "/static/media/twitter.08a1258d.png"
+  }
+]);
